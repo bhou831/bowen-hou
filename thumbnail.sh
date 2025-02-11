7c6fc506d477e9f9a2c0199f7360d88cb5ab6aff
bhou831 <bowenhou@andrew.cmu.edu>
2025-02-10 23:23:25 -0500

    update photo collection

--- thumbnail.sh
+++ thumbnail.sh
@@ -7,6 +7,7 @@ output="${input%.*}_thumb.jpg"
 
 # Generate thumbnail with max dimensions 700x500
 ffmpeg -i "$input" \
-    -vf "scale='if(gt(iw,ih),min(700,iw),-1)':if(gt(ih,iw),min(500,ih),-1)" \
+    -vf "scale=w=1920:h=1080:force_original_aspect_ratio=decrease" \
     -quality 85 \
+    -y \
     "$output"
